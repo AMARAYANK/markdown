22aea38d79f4d18153478f558358119bfa431af9
amarayank <amarayank4@gmail.com>
2021-11-15 12:06:14 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, {useState} from 'react';
+import {marked} from 'marked';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 export default function App() {
   const [markdown, setMarkdown] = useState('Suuppp')
 
+  function handleChange(e){
+      setMarkdown(e.target.value)
+  }
+
   return (
     <div className="app">
       <textarea 
         value = {markdown}
-        onChange = {e => setMarkdown(e.target.value)}
+        onChange = {handleChange}
       />
-
-      <div className="preview">{markdown}</div>
+      <ReactMarkdown className='preview' children={markdown} /> 
     </div>
   );
 }
