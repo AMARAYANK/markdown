2f1ac4ed797b16086eae13f36edd91081e104fc4
amarayank <amarayank4@gmail.com>
2021-11-15 11:12:06 +0530

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [markdown, setMarkdown] = useState('Suuppp')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <textarea 
+        value = {markdown}
+        onChange = {e => setMarkdown(e.target.value)}
+      />
+
+      <div className="preview">{markdown}</div>
     </div>
   );
 }
-
-export default App;
